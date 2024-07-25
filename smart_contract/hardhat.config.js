2b57d57bbd6389b17605396382d7c6b4918389df
guesmi-samia123 <55672910+guesmi-samia123@users.noreply.github.com>
2024-07-25 17:31:01 +0200

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,16 +5,35 @@ const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.24",
-  settings: {
-    modelChecker: {
-      engine: 'all',
-      showUnprouved: true
 
+module.exports = {
+
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      modelChecker: true,
+      modelChecker: {
+        engine: "all", // or "all", "bmc", "kle", "none"
+        showUnproved: true,
+        showProvedSafe : true,
+        showUnsupported : true,
+      }
     }
   },
-  networks: {
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+
+
+  
+networks: {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts:[SEPOLIA_PRIVATE_KEY]  
